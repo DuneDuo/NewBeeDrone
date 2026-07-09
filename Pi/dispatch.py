f339e0d3e8e4ae718bb9ac5d259bfbb7e918599c
DuneDuo <example_more@live.cn>
2026-07-09 20:07:17 +0800

    dispatch用字典分发

---
+++ Pi/dispatch.py
@@ -0,0 +1,15 @@
+from logger import logger
+log = logger("dispatch")
+_handlers = {}
+def register(msg_type):
+    def wrapper(func):
+        _handlers[msg_type] = func
+        return func
+    return wrapper
+def dispatch(msg):
+    msg_type = msg.get_type()
+    handler = _handlers.get(msg_type)
+    if handler:
+        handler(msg)
+    else:
+        log.info(f"未注册的消息:{msg_type}")
